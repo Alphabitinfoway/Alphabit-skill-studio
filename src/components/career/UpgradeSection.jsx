cd5e4b5021650194d036c5abc137fa3a2c90f6c4
Dhruv Limbasiya <24031701027@darshan.ac.in>
2026-05-11 09:50:11 +0530

    font size changed according to design

--- src/components/career/UpgradeSection.jsx
+++ src/components/career/UpgradeSection.jsx
@@ -1,7 +1,7 @@
 export default function UpgradeSection() {
   return (
     <section className="w-full text-center py-20 px-4">
-      <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4 font-cabinet">
+      <h2 className="text-2xl md:text-3xl font-bold text-gray-900 mb-4 font-cabinet">
         <span className="text-[#FF5722] font-pp italic font-medium text-5xl md:text-6xl">Upgrade</span> Your Technical Trajectory for<br /> High-Value Enterprise Roles
       </h2>
       <p className="text-gray-500 mx-auto text-base" style={{ maxWidth: "860px" }}>
